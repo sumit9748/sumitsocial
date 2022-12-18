9d89d9f9c7143474bb6be8bc5e67d08468b0d3dc
sumit <sumitmondal9448@gmail.com>
2022-12-18 14:33:51 +0530

    firebase

--- src/pages/Firebase.js
+++ src/pages/Firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
-import { getStorage } from 'firebase/storage'
+import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-    apiKey: "AIzaSyBauuA63lmMA9XFHRIUtRUNZ8pLt9g3Nek",
-    authDomain: "socialmeo-c671e.firebaseapp.com",
-    projectId: "socialmeo-c671e",
-    storageBucket: "socialmeo-c671e.appspot.com",
-    messagingSenderId: "1008548068290",
-    appId: "1:1008548068290:web:84d201514c829e3c5f345b"
+  apiKey: "AIzaSyBauuA63lmMA9XFHRIUtRUNZ8pLt9g3Nek",
+  authDomain: "socialmeo-c671e.firebaseapp.com",
+  projectId: "socialmeo-c671e",
+  storageBucket: "socialmeo-c671e.appspot.com",
+  messagingSenderId: "1008548068290",
+  appId: "1:1008548068290:web:84d201514c829e3c5f345b",
 };
 
 // Initialize Firebase
